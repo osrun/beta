a98d0ffecd22f6087ae9b80eb7f83184214dcc1b
osrun <44208422+osrun@users.noreply.github.com>
2019-03-28 09:54:25 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -49,8 +49,7 @@ case $1 in
    if ifconfig | grep "inet addr" | cut -d: -f2 | cut -dB -f1 | cut -d. -f1 | grep -o 192 > /dev/null; then
     _APP start
    fi
-   #read -p "PRESS [ENTER]" NULL
-   #setsid cttyhack /bin/login
-   /bin/login
+   tput civis; read -p "PRESS [ENTER]" NULL; tput cnorm
+   setsid cttyhack /bin/login   
 ;;
 esac
